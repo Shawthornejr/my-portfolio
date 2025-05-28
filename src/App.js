9af26db8cdbef4da7bd1e53a1cc30e7c24a971f5
Shawthornejr <samuelrhawthorne32@gmail.com>
2025-05-28 05:35:11 -0500

    Initial portfolio setup with clean React app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My Portfolio</h1>
+      <p>This site is built with React!</p>
+    </div>
+  );
+}
+
+export default App;
+
